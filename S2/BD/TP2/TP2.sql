43c3cfef8dd9615f21a8697bd5a7d9469e70b691
Ashley <59657132+sillyash@users.noreply.github.com>
2024-02-12 17:51:59 +0100

    Update TP2.sql

--- S2/BD/TP2/TP2.sql
+++ S2/BD/TP2/TP2.sql
@@ -1,1 +1,70 @@
+// a)
+SELECT DISTINCT numvoyage, nomvoyage
+FROM Voyage
+NATURAL JOIN Etape
+NATURAL JOIN Ville
+NATURAL JOIN Pays
+WHERE nompays = 'France'
+ORDER BY numvoyage;
+
+
+// b)
+SELECT DISTINCT numvoyage, nomvoyage
+FROM Voyage
+NATURAL JOIN Etape
+NATURAL JOIN Ville
+NATURAL JOIN Pays
+WHERE numvoyage NOT IN (SELECT numvoyage
+                        FROM Etape
+                        NATURAL JOIN Ville
+                        NATURAL JOIN Pays
+                        WHERE nompays != 'France')
+ORDER BY numvoyage;
+
+
+// c)
+SELECT DISTINCT numvoyage, nomvoyage
+FROM Voyage
+NATURAL JOIN Etape
+NATURAL JOIN Ville
+NATURAL JOIN Pays
+WHERE numvoyage IN (SELECT numvoyage
+                    FROM Etape
+                    NATURAL JOIN Ville
+                    NATURAL JOIN Pays
+                    WHERE nompays != 'France')
+ORDER BY numvoyage;
+
+
+// d)
+SELECT DISTINCT numvoyage, nomvoyage
+FROM Voyage
+NATURAL JOIN Etape
+NATURAL JOIN Ville
+NATURAL JOIN Pays
+WHERE nompays != 'France'
+ORDER BY numvoyage;
+
+
+// e)
+SELECT DISTINCT VG.nomvoyage, V.nomville, E.duree
+FROM Voyage VG
+INNER JOIN Etape E ON E.numvoyage = VG.numvoyage
+INNER JOIN Ville V ON V.numville = E.numville
+WHERE E.duree = (SELECT MAX(duree)
+                FROM Etape E
+                WHERE VG.numvoyage = E.numvoyage);
+
+
+// f)
+SELECT VG.nomvoyage
+FROM Voyage VG
+WHERE (SELECT COUNT (*)
+        FROM Etape E
+        INNER JOIN Ville V ON E.numville = V.numville
+        WHERE etat = 'Californie')
+        > (SELECT COUNT (*)
+           FROM Etape E
+           INNER JOIN Ville V ON E.numville = V.numville
+           WHERE etat = 'Arizona');
 
