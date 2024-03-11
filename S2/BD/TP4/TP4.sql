9170bc046026a4d2fcacde38953a226810802f48
Ashley <59657132+sillyash@users.noreply.github.com>
2024-03-11 11:05:34 +0100

    Update TP4.sql

--- S2/BD/TP4/TP4.sql
+++ S2/BD/TP4/TP4.sql
@@ -10,7 +10,32 @@ ORDER BY idFilm;
 // b)
 SELECT P.prenomPersonne, P.nomPersonne
 FROM bdfilm.Personne P
-WHERE P.idPersonne NOT IN (select * from bdfilm.EquipeFilm EF where EF.job = 'Director')
-AND P.idPersonne NOT IN bdfilm.Rolefilm;
+WHERE P.idPersonne NOT IN (select idpersonne from bdfilm.EquipeFilm EF where EF.job = 'Director')
+AND P.idPersonne NOT IN (select idpersonne from bdfilm.RoleFilm);
 
-select * from bdfilm.equipefilm;
+// c)
+SELECT P.prenomPersonne, P.nomPersonne
+FROM bdfilm.Personne P
+WHERE (select count(idpersonne) from bdfilm.EquipeFilm EF where EF.job = 'Director' and idpersonne = P.idpersonne)
+        > (select count(idpersonne) from bdfilm.RoleFilm where idpersonne = P.idpersonne);
+        
+// d)
+SELECT DISTINCT P.prenomPersonne, P.nomPersonne
+FROM bdfilm.Personne P
+INNER JOIN bdfilm.EquipeFilm EF ON EF.idPersonne = P.idPersonne
+INNER JOIN bdfilm.Film F ON EF.idFilm = F.idFilm
+WHERE P.idPersonne IN (select idPersonne from bdfilm.RoleFilm where idFilm = F.idFilm);
+
+
+// Exercice 2
+// a)
+DROP VIEW vFilm;
+
+CREATE VIEW vFilm AS
+SELECT idFilm, (select count (*) from bdfilm.sortie where idFilm = F.idFilm) AS NbSorties
+FROM bdfilm.Film F
+ORDER BY idFilm;
+
+SELECT * FROM vFilm;
+
+
