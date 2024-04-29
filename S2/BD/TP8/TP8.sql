cb7bbf6178244cc665ecb0360703e7e7c730975d
Ashley <59657132+sillyash@users.noreply.github.com>
2024-04-29 15:25:13 +0200

    Update TP8.sql

--- S2/BD/TP8/TP8.sql
+++ S2/BD/TP8/TP8.sql
@@ -15,14 +15,168 @@ BEGIN
 END nbFilms;
 /
 
-EXECUTE DBMS_OUTPUT.PUT_LINE(nbFilms(880)||' films.');
+EXECUTE DBMS_OUTPUT.PUT_LINE('L"acteur n°880 a joué dans '||nbFilms(880)||' film(s).');
 
 // b)
 
 CREATE OR REPLACE PROCEDURE Afficher
-    (acteur NUMBER)
+    (idActeur bdfilm.personne.idPersonne%type)
+IS
+nomActeur bdfilm.personne.nomPersonne%type;
+prenomActeur bdfilm.personne.prenomPersonne%type;
+CURSOR curseurActGenre IS
+    SELECT G.idGenre, G.nomGenre, COUNT (*) AS NbFilmsGenre
+    FROM bdfilm.RoleFilm RF
+    INNER JOIN bdfilm.Film F ON F.idFilm = RF.idFilm
+    INNER JOIN bdfilm.GenreFilm GF ON GF.idFilm = F.idFilm
+    INNER JOIN bdfilm.Genre G ON G.idGenre = GF.idGenre
+    WHERE idPersonne = idActeur
+    GROUP BY G.idGenre, G.nomGenre;
 BEGIN
-    SELECT *
-    FROM bdfilm.Film F
+    SELECT P.PrenomPersonne, P.NomPersonne INTO prenomActeur, nomActeur
+    FROM bdfilm.Personne P
+    WHERE idPersonne = idActeur;
+    DBMS_OUTPUT.PUT_LINE(prenomActeur||' '||nomActeur||' a joué dans dans ces genres :');
+    FOR ligneC IN curseurActGenre
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'idGenre : '||ligneC.idGenre||CHR(9)||
+            'Nom du genre : '||ligneC.nomGenre||CHR(9)||
+            'Nombre de films : '||ligneC.NbFilmsGenre);
+    END LOOP;
 END Afficher;
 /
+
+EXECUTE Afficher(880);
+
+
+// c)
+
+CREATE OR REPLACE PROCEDURE AfficherNom
+    (nomActeur bdfilm.Personne.NomPersonne%type)
+IS
+CURSOR acteurNom IS
+    SELECT idPersonne
+    FROM bdfilm.Personne P
+    WHERE nomPersonne = nomActeur;
+BEGIN
+    FOR ligneC IN acteurNom
+    LOOP
+        Afficher(ligneC.idPErsonne);
+        DBMS_OUTPUT.PUT_LINE('');
+    END LOOP;
+END AfficherNom;
+/
+
+EXECUTE AfficherNom('Affleck');
+
+
+// d)
+
+CREATE OR REPLACE PROCEDURE AfficherNom
+    (nomActeur bdfilm.Personne.NomPersonne%type)
+IS
+nbreFilms NUMBER;
+CURSOR acteurNom IS
+    SELECT idPersonne
+    FROM bdfilm.Personne P
+    WHERE nomPersonne = nomActeur;
+BEGIN
+    FOR ligneC IN acteurNom
+    LOOP
+        nbreFilms := nbFilms(ligneC.idPersonne);
+        Afficher(ligneC.idPErsonne);
+        DBMS_OUTPUT.PUT_LINE('Nombre total de films joués : '||nbreFilms);
+        DBMS_OUTPUT.PUT_LINE('');
+    END LOOP;
+END AfficherNom;
+/
+
+EXECUTE AfficherNom('Affleck');
+
+
+// Exercice 2
+// a)
+
+CREATE OR REPLACE PROCEDURE ActeursDe
+    (film bdfilm.Film.idFilm%type)
+IS
+nbreFilms NUMBER;
+titreFilm bdfilm.film.titreOriginal%type;
+CURSOR acteursFilm IS
+    SELECT nomPersonne, prenomPersonne
+    FROM bdfilm.Film F
+    INNER JOIN bdfilm.RoleFilm RF ON RF.idFilm = F.idFilm
+    INNER JOIN bdfilm.Personne P ON P.idPersonne = RF.idPErsonne
+    WHERE F.idFilm = film;
+BEGIN
+    SELECT titreOriginal INTO titreFilm
+    FROM bdfilm.Film F
+    WHERE F.idFilm = film;
+    DBMS_OUTPUT.PUT_LINE('Acteurs qui jouent dans "'||titreFilm||'" :');
+    DBMS_OUTPUT.PUT_LINE('');
+    FOR ligne IN acteursFilm
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Nom : '||ligne.nomPersonne||CHR(9)||
+            'Prénom : '||ligne.prenomPersonne);
+    END LOOP;
+END ActeursDe;
+/
+
+EXECUTE ActeursDe(489);
+
+
+// b)
+
+CREATE OR REPLACE PROCEDURE ActeursDuFilm
+    (titreFilm bdfilm.Film.titre%type)
+IS
+nbreFilms NUMBER;
+numFilm bdfilm.film.idFilm%type;
+BEGIN
+    SELECT idFilm INTO numFilm
+    FROM bdfilm.Film F
+    WHERE F.titre = titreFilm;
+    DBMS_OUTPUT.PUT_LINE('Le film "'||titreFilm||'" est le film n°'||numFilm||'.');
+    DBMS_OUTPUT.PUT_LINE('');
+    ActeursDe(numFilm);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('Il n''y a pas de film avec ce titre.');
+    WHEN TOO_MANY_ROWS THEN
+    DBMS_OUTPUT.PUT_LINE('Plusieurs films avec ce titre !');
+END ActeursDuFilm;
+/
+
+EXECUTE ActeursDuFilm('Will Hunting');
+EXECUTE ActeursDuFilm('Arizona Dream');
+EXECUTE ActeursDuFilm('La vie est belle');
+
+
+// c)
+
+CREATE OR REPLACE PROCEDURE ActeursDuFilm
+    (titreFilm bdfilm.Film.titre%type)
+IS
+nbreFilms NUMBER;
+numFilm bdfilm.film.idFilm%type;
+BEGIN
+    SELECT idFilm INTO numFilm
+    FROM bdfilm.Film F
+    WHERE F.titre = titreFilm;
+    DBMS_OUTPUT.PUT_LINE('Le film "'||titreFilm||'" est le film n°'||numFilm||'.');
+    DBMS_OUTPUT.PUT_LINE('');
+    ActeursDe(numFilm);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('Il n''y a pas de film avec ce titre.');
+    WHEN TOO_MANY_ROWS THEN
+    DBMS_OUTPUT.PUT_LINE('Plusieurs films avec ce titre !');
+END ActeursDuFilm;
+/
+
+EXECUTE ActeursDuFilm('Will');
+EXECUTE ActeursDuFilm('Arizona');
+EXECUTE ActeursDuFilm('Dream');
+
