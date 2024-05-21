209f9365d5051366ef0037f0ee2d6674f09651e2
Ashley <59657132+sillyash@users.noreply.github.com>
2024-05-21 08:35:19 +0200

    Add files via upload

--- S2/ARCHI/TP_ASM/Polynome2int/main.cpp
+++ S2/ARCHI/TP_ASM/Polynome2int/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+extern "C" double polynome2int(double a, double b, double c, double x);
+
+int main()
+{
+	double a, b, c, x;
+
+	cout << endl << "Ce programme sert a calculer la valeur d'un polynome"
+		<< "entier de second degre a * n ^ 2 + b * n + c" << endl << endl;
+
+	cout << "Entrez une valeur entiere pour a : "; cin >> a;
+	cout << "Entrez une valeur entiere pour b : "; cin >> b;
+	cout << "Entrez une valeur entiere pour c : "; cin >> c;
+	cout << "Entrez une valeur entiere pour n : "; cin >> x;
+
+	cout << endl << "Pour n = " << x << " : " << a << "*n^2 + " << b
+		<< "*n + " << c << " = " << polynome2int(a, b, c, x) << endl;
+
+	return 0;
+}
