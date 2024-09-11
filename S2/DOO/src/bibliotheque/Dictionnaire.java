962ea91ef188c4a49f8cec9b51d5b7fed443aa6c
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-09-11 13:00:09 +0200

    New Eclipse Workspace version change

--- S2/DOO/src/bibliotheque/Dictionnaire.java
+++ S2/DOO/src/bibliotheque/Dictionnaire.java
@@ -3,8 +3,14 @@ package bibliotheque;
 public class Dictionnaire extends Document {
 
 	protected String langue;
+	
+	@SuppressWarnings("unused")
 	private final String ANG = "anglais";
+	
+	@SuppressWarnings("unused")
 	private final String ALL = "allemand";
+	
+	@SuppressWarnings("unused")
 	private final String ESP = "espagnol";
 
 }
