fddda0558ec2bf7516dbfae3c6c2f848390aa8c5
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-09-25 13:16:30 +0200

    LGTM

--- S3-APP/R302/TD1_2/Main.java
+++ S3-APP/R302/TD1_2/Main.java
@@ -7,13 +7,13 @@ public class Main {
         System.out.println(sep + "Test List" + sep);
 
         List li = new List(1, 2, 3, 4);
-        System.out.println("Initial list :\t\t" + li);
+        System.out.println("Initial list :\t\t\t" + li);
 
         li.addFirstElement(0);
         System.out.println("addFirstElement(0) :\t" + li);
 
         li.addEndElement(5);
-        System.out.println("addEndElement(5) :\t" + li);
+        System.out.println("addEndElement(5) :\t\t" + li);
 
         System.out.println();
         System.out.println("\n" + sep + "Test BinaryTree" + sep);
