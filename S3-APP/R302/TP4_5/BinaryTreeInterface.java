c6a718ea862928f54e628064094d53115703b47d
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-10-02 15:29:20 +0200

    AAAAAAAAA UwU

---
+++ S3-APP/R302/TP4_5/BinaryTreeInterface.java
@@ -0,0 +1,19 @@
+public interface BinaryTreeInterface<T> extends TreeInterface<T>{ 
+    public T data();
+    public BinaryTree<T> parent();
+    public BinaryTree<T> left();
+    public void setLeft(BinaryTree<T> t);
+    public BinaryTree<T> right();
+    public void setRight(BinaryTree<T> t);
+    public String toString();
+    public void display();
+
+    /* Les méthodes suivantes sont ajoutées pour que les arbres
+     * binaires BinaryTree fonctionnent pour les fonctions acceptant des 
+     * arbres Tree mais ne sont pas utiles si l'arbre est binaire.
+     */
+    public Tree<T> child(int i);
+    public int nbChildren();
+    public void addChildren(@SuppressWarnings("unchecked") BinaryTree<T>... childs);
+    public void setChild(int i, BinaryTree<T> child);
+}
