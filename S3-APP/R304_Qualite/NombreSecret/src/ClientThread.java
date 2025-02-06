154eb8a260805ca5fe3bd7a1f761f75707833c8c
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-02-06 14:43:30 +0100

    Moved Qualite to qualite directory bc im fucking stupid

---
+++ S3-APP/R304_Qualite/NombreSecret/src/ClientThread.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientThread extends Thread
+{
+    protected Socket socket;
+    protected PrintWriter writeData;
+    protected BufferedReader readData;
+
+    public ClientThread(Socket s) throws IOException {
+        this.socket = s;
+        this.writeData = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.readData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        System.out.println(
+                "Secret number game : find the number between " +
+                Server.MIN_NUM + " and " + Server.MAX_NUM
+        );
+
+        Scanner sc = new Scanner(System.in);
+        String response = "";
+
+        System.out.println("Waiting for another player... ");
+
+        try {
+            do {
+                response = readData.readLine().strip();
+            } while (!response.equals("GO"));
+
+            System.out.println("Found!");
+
+            do {
+                System.out.print(
+                        "Please input a number between " + Server.MIN_NUM +
+                         " and " + Server.MAX_NUM + " >>> "
+                );
+
+                int guessedNum = sc.nextInt();
+                writeData.println(guessedNum);
+                writeData.flush();
+
+                response = readData.readLine().strip();
+                System.out.println(response);
+            } while (!response.equals("Congratulations, you found the number !"));
+        } catch (IOException e) {
+            System.err.println("Failed to read buffer : " + e.getMessage());
+        }
+
+        try {
+            response = readData.readLine().strip();
+            System.out.println(response);
+            sc.close();
+            writeData.close();
+            readData.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Failed to close buffer/connection : " + e.getMessage());
+        }
+    }
+}
---
+++ S3-APP/R304_Qualite/NombreSecret/src/ServerThread.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class ServerThread extends Thread
+{
+    protected Socket socket;
+    protected PrintWriter writeData;
+    protected BufferedReader readData;
+    protected int secretNum;
+    protected int nbTries;
+
+    public ServerThread(Socket s, int secretNum) throws IOException {
+        this.socket = s;
+        this.secretNum = secretNum;
+        this.readData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writeData = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.nbTries = 0;
+    }
+
+    public int getNbTries() {
+        return this.nbTries;
+    }
+
+    @Override
+    public void run() {
+        writeData.println("GO");
+        writeData.flush();
+
+        String read = "";
+        int guessedNum = Integer.MIN_VALUE;
+
+        try {
+            do {
+                read = readData.readLine().strip();
+                try {
+                    guessedNum = Integer.parseInt(read);
+                } catch (NumberFormatException e) {
+                    System.err.println("Received \"" + read + "\" : not a number.");
+                    System.err.println(e.getMessage());
+                    return;
+                }
+
+                if (guessedNum > secretNum) {
+                    writeData.println("Lower!");
+                }
+                else if (guessedNum < secretNum) {
+                    writeData.println("Higher!");
+                }
+                nbTries++;
+                writeData.flush();
+
+            } while (guessedNum != secretNum);
+        } catch (IOException e) {
+            System.err.println("Failed to read buffer : " + e.getMessage());
+        }
+
+        writeData.println("Congratulations, you found the number !");
+        writeData.flush();
+        writeData.println("Number of tries : " + nbTries);
+        writeData.flush();
+
+        try {
+            readData.close();
+            writeData.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Failed to close buffer/connection : " + e.getMessage());
+        }
+    }
+}
