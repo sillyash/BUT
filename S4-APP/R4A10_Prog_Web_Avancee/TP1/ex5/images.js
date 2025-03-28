b31dac2abf9cbed533798685405cbd442ba80d54
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2025-03-28 10:10:29 +0100

    ex5 done

--- S4-APP/R4A10_Prog_Web_Avancee/TP1/ex5/images.js
+++ S4-APP/R4A10_Prog_Web_Avancee/TP1/ex5/images.js
@@ -2,17 +2,48 @@ const filename = "images.js";
 console.info("File \"" + filename + "\" loaded.");
 
 function setImages() {
-	let divs = document.querySelectorAll('div[id^=cadre]');
+	let divs = document.querySelectorAll('.asideImages > div');
 	
 	divs.forEach(
 		d => {
 			const uri = `url(./images/filtres_et_cadres/${d.id}.png)`;
 			d.style.backgroundImage = uri;
-			d.style.height = '150px';
+
+			d.style.height = '250px';
+			d.style.width = '200px';
+
 			d.style.backgroundSize = 'contain';
 			d.style.backgroundRepeat = 'no-repeat';
+			
+			d.style.position = 'relative';
+			d.style.top = 0;
+			d.style.left = 0;
+
+			d.addEventListener('mousemove', move_img);
 		}
 	);
+}
+
+/**
+ * @param {Event} e 
+ */
+function move_img(e) {
+	// Return if no left click
+	const btns = e.buttons;
+	if ( ! btns == 1) return;
+	
+	let div = e.target;
+	const mouseX = e.movementX;
+	const mouseY = e.movementY;
+
+	const divX = parseInt(div.style.left.slice(0, -2));
+	const divY = parseInt(div.style.top.slice(0, -2));
+	
+	const totalX = divX + mouseX;
+	const totalY = divY + mouseY;
+
+	div.style.left = totalX + 'px';
+	div.style.top = totalY + 'px';
 }
 
 /* -------------------- Main -------------------- */
