96adfe205fb31508ee423072da7f2301a12bba3c
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2026-03-24 10:57:31 +0100

    feat: Q2 TP5 R5A04

--- S6-APP/R5A04_Analyse_perf_algo/TP5/profile_insertSort.py
+++ S6-APP/R5A04_Analyse_perf_algo/TP5/profile_insertSort.py
@@ -1,11 +1,21 @@
 import random
+from line_profiler import profile
 
+@profile
 def insertSort(tab):
     result = []
     for elt in tab:
         result = insertInOrder(elt, result)
     return result
 
+@profile
+def insertSort_rec(tab):
+    result = []
+    for elt in tab:
+        result = insertInOrder_rec(elt, result)
+    return result
+
+@profile
 def insertInOrder(elt, tab):
     i = 0
     while (i < len(tab)):
@@ -15,10 +25,11 @@ def insertInOrder(elt, tab):
     tab.insert(i, elt)
     return tab
 
+@profile
 def random_tab(value, length):
         return [random.randint(1, value) for _ in range(length)]
 
-
+@profile
 def insertInOrder_rec(elt, tab):
     if (tab == []):
         return [elt]
@@ -29,7 +40,8 @@ def insertInOrder_rec(elt, tab):
         return insertInOrder_rec(elt, tab[:mid]) + tab[mid:]
 
 def main():
-    tab = random_tab(10000, ____)
-    print(insertSort(tab))
+    tab = random_tab(10000, 1000)
+    insertSort(tab)
+    insertSort_rec(tab)
 
 main()
